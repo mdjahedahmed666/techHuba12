df2f139c22ead4beb71bd11c03186a0884ea12a0
Jahed <mdjahedahmed666@gmail.com>
2023-11-26 01:05:21 +0000

    logedin user name and profile photo is added in navbar

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,43 +1,105 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { BiUpvote } from "react-icons/bi";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const FeaturedProducts = () => {
-    const [featuredProducts, setFeaturedProducts] = useState([]);
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+  // const [upvotes, setUpvotes] = useState([]);
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-      fetch("http://localhost:5000/featuredProducts")
-        .then((res) => res.json())
-        .then((data) => setFeaturedProducts(data));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/featuredProducts")
+      .then((res) => res.json())
+      .then((data) =>  {
+        // Convert timestamp strings to numerical values
+        const productsWithNumericalTimestamp = data.map((product) => ({
+          ...product,
+          timestamp: new Date(product.timestamp).getTime(),
+        }));
+
+        // Sort the products by timestamp in descending order (latest first)
+        const sortedProducts = productsWithNumericalTimestamp.sort(
+          (a, b) => b.timestamp - a.timestamp
+        );
+
+        setFeaturedProducts(sortedProducts);
+      });
+  }, []);
+
+  const handleUpvote = (productId) => {
+    if (user) {
+      // Find the product by ID
+      const updatedProducts = featuredProducts.map((product) => {
+        if (product.id === productId) {
+          const newUpvotes = product.upvotes + 1;
+
+          // Update the product's upvotes locally
+          return { ...product, upvotes: newUpvotes };
+        }
+
+        return product;
+      });
+
+      // Update the state with the new upvotes
+      setFeaturedProducts(updatedProducts);
+
+      // Send data to the server
+      // fetch(`http://localhost:5000/upvotes/${productId}`, {
+      //   method: "PUT",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({ upvotes: updatedProducts.find(product => product.id === productId).upvotes }),
+      // })
+      //   .then((res) => res.json())
+      //   .then((data) => {
+      //     console.log(data);
+      //   })
+      //   .catch((error) => {
+      //     console.error("Error:", error);
+      //     // Show an error notification using SweetAlert2
+      //     Swal.fire({
+      //       icon: "error",
+      //       title: "Upvote Error",
+      //       text: "There was an error processing your upvote. Please try again.",
+      //     });
+      //   });
+    }
+  };
   return (
     <div className="container mx-auto px-4 md:px-32  my-28 py-10">
       <h2 className="font-bold font-rancho text-2xl text-black text-center mb-10">
         Featured Products
       </h2>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
-      {featuredProducts.map((product, index) => (
-        <div
-          key={index}
-          className="card card-compact bg-base-100 shadow-xl"
-        >
-          <figure>
-            <img
-              src={product.image}
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-          <Link to="/productDetails" className="card-title underline">
-            <h2 >{product.name}</h2>
-            </Link>
-            <p>{product.tags.join(', ')} </p>  
+        {featuredProducts.map((product, index) => (
+          <div key={index} className="card card-compact bg-base-100 shadow-xl">
+            <figure>
+              <img src={product.image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <Link to="/productDetails" className="card-title underline">
+                <h2>{product.name}</h2>
+              </Link>
+              <p>{product.tags.join(", ")} </p>
+              <div>
+                <button
+                  onClick={() => handleUpvote(product._id)}
+                  className={user ? "btn btn-circle" : "btn btn-circle"}
+                  disabled={!user}
+                >
+                  <BiUpvote />
+                </button>{" "}
+                <span>{product.upvotes}</span>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default FeaturedProducts
+export default FeaturedProducts;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,51 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 import Swal from "sweetalert2";
 
 const Navbar = () => {
-    const { user, logOut } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [logedUser, setLogedUser] = useState([]);
+  const { user, logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleLogOut = () => {
-        logOut()
-          .then(
-            Swal.fire({
-              title: "Log Out",
-              text: "Successfully logged out",
-              icon: "Success",
-              confirmButtonText: "ok",
-            }),
-            navigate("/")
-          )
-          .catch((error) => {
-            console.error("Logout failed", error);
-          });
-      };
+  useEffect(() => {
+    if (user && user?.email) {
+      fetch('http://localhost:5000/users')
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const matchedUser = data.find(
+            (userData) => userData.email === user.email
+          );
+          console.log(matchedUser);
+          if (matchedUser) {
+            setLogedUser(matchedUser);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    } else {
+      // Set userData to a default value or an empty object
+      setLogedUser([]);
+    }
+  }, [user]);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(
+        Swal.fire({
+          title: "Log Out",
+          text: "Successfully logged out",
+          icon: "Success",
+          confirmButtonText: "ok",
+        }),
+        navigate("/")
+      )
+      .catch((error) => {
+        console.error("Logout failed", error);
+      });
+  };
   const navLinks = (
     <>
       <li className="text-lg">
@@ -30,13 +54,13 @@ const Navbar = () => {
       <li className="text-lg">
         <NavLink to="/products">Products</NavLink>
       </li>
-      {user?
-      ""
-      :
-      <li className="text-lg">
-      <NavLink to="/login">Login</NavLink>
-    </li>
-      }
+      {user ? (
+        ""
+      ) : (
+        <li className="text-lg">
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      )}
     </>
   );
   return (
@@ -66,7 +90,9 @@ const Navbar = () => {
             {navLinks}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost text-xl">TechHub</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          TechHub
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navLinks}</ul>
@@ -77,7 +103,7 @@ const Navbar = () => {
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
-                src="https://i.ibb.co/NN3pKTD/jahedpp-removebg-preview.png"
+                src={logedUser.photo}
               />
             </div>
           </label>
@@ -86,13 +112,15 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <p className="justify-between">Md jahed Miah</p>
+              <p className="justify-between">{logedUser.name}</p>
             </li>
             <li>
               <Link to="/dashboard">Dashboard</Link>
             </li>
             <li>
-              <Link onClick={handleLogOut} to="/login">Logout</Link>
+              <Link onClick={handleLogOut} to="/login">
+                Logout
+              </Link>
             </li>
           </ul>
         </div>
